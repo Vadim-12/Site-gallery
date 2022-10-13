3c07ba5e4a235721ac8e3cbd33e8b34f27b15e7a
Vadim-12 <66643471+Vadim-12@users.noreply.github.com>
2022-10-13 10:41:35 +0300

    add name search

--- js/script.js
+++ js/script.js
@@ -1,52 +1,65 @@
-/*Vue.component('app-good',{
+Vue.component('app-good',{
+	props: ['title','author','img_href','price_good','price_bad','is_sold'],
 	template: `
-		<div class="good" v-for="good in goods">
-			<img class="pic" src="../img/pic1.png" alt="pic">
+		<div class="good" :class="{ 'sold-in-auction': is_sold }">
+			<img class="pic" v-bind:src=img_href alt="pic">
 			<div class="good-content">
 				<h2>
-					«{{ good.title }}»<br>
-					{{ good.author }}
+					«{{ title }}»<br>
+					{{ author }}
 				</h2>
-				<div class="buy-block">
+				<div class="buy-block" v-show="!is_sold">
 					<div class="price-block">
-						<div class="price-bad price-one">
-							2&nbsp;000&nbsp;000&nbsp;$
+						<div class="price-bad price-one" v-show="price_bad != null">
+							{{ price_bad }} $
 						</div>
-						<div class="price-good price-one">
-							1&nbsp;000&nbsp;000&nbsp;$
+						<div class="price-good price-one" v-show="price_good != null">
+							{{ price_good }} $
 						</div>
 					</div>
 					<button class="buy-btn">Купить</button>
 				</div>
+				<div class="buy-block" v-show="is_sold">
+					Продана на аукционе
+				</div>
 			</div>
 		</div>
-	`,
-	data() {
-		return {
-			goods: [
-				{id: 1, title: 'Рождение Венеры', author: 'Сандро Боттичелли', price_good: 1000000, price_bad: 2000000, is_sold: false},
-				{id: 2, title: 'Тайная вечеря', author: 'Леонардо да Винчи', price_good: 3000000, is_sold: false},
-				{id: 3, title: 'Сотворение Адама', author: 'Микеланджело', price_good: 5000000, price_bad: 6000000, is_sold: false},
-				{id: 4, title: 'Урок анатомии', author: 'Рембрандт', is_sold: false}
-			]
-		}
-	}
-});*/
+	`
+});
 
 let app = new Vue({
 	el: '#app',
-	data: {
-		is_active: false,
-		items: [
-			{id: 1, img: '../img/pic1.png', title: 'Рождение Венеры', author: 'Сандро Боттичелли', price_good: 1000000, price_bad: 2000000, is_sold: false},
-			{id: 2, img: '../img/pic2.png', title: 'Тайная вечеря', author: 'Леонардо да Винчи', price_good: 3000000, price_bad: null, is_sold: false},
-			{id: 3, img: '../img/pic3.png', title: 'Сотворение Адама', author: 'Микеланджело', price_good: 5000000, price_bad: 6000000, is_sold: false},
-			{id: 4, img: '../img/pic4.png', title: 'Урок анатомии', author: 'Рембрандт', price_good: null, price_bad: null, is_sold: true}
-		]
+	data() {
+		return {
+			is_mobile_menu_active: false,
+			items: [
+				{id: 1, title: 'Рождение Венеры', author: 'Сандро Боттичелли', img_href: '../img/pic1.png', price_good: 1000000, price_bad: 2000000, is_sold: false},
+				{id: 2, title: 'Тайная вечеря', author: 'Леонардо да Винчи', img_href: '../img/pic2.png', price_good: 3000000, is_sold: false},
+				{id: 3, title: 'Сотворение Адама', author: 'Микеланджело', img_href: '../img/pic3.png', price_good: 5000000, price_bad: 6000000, is_sold: false},
+				{id: 4, title: 'Урок анатомии', author: 'Рембрандт', img_href: '../img/pic4.png', is_sold: true}
+			],
+			showItems: [
+				{id: 1, title: 'Рождение Венеры', author: 'Сандро Боттичелли', img_href: '../img/pic1.png', price_good: 1000000, price_bad: 2000000, is_sold: false},
+				{id: 2, title: 'Тайная вечеря', author: 'Леонардо да Винчи', img_href: '../img/pic2.png', price_good: 3000000, is_sold: false},
+				{id: 3, title: 'Сотворение Адама', author: 'Микеланджело', img_href: '../img/pic3.png', price_good: 5000000, price_bad: 6000000, is_sold: false},
+				{id: 4, title: 'Урок анатомии', author: 'Рембрандт', img_href: '../img/pic4.png', is_sold: true}
+			],
+			query: ''
+		}
 	},
 	methods: {
 		toggle_mobile_menu: function(){
-			this.is_active = !this.is_active;
+			this.is_mobile_menu_active = !this.is_mobile_menu_active;
+		},
+		updateItems: function(){
+			console.log('ура');
+			this.showItems = this.queryItems;
+		}
+	},
+	computed: {
+		queryItems: function(){
+			console.log(this.query);
+			return this.items.filter((item) => item.title.toLowerCase().indexOf(this.query.toLowerCase()) != -1);
 		}
 	}
 });
