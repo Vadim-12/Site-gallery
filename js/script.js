7df587dbb91a72ac27e431373f9063aec87c1519
Vadim-12 <66643471+Vadim-12@users.noreply.github.com>
2022-10-13 13:13:39 +0300

    do dynamic search

--- js/script.js
+++ js/script.js
@@ -38,22 +38,12 @@ let app = new Vue({
 				{id: 3, title: 'Сотворение Адама', author: 'Микеланджело', img_href: '../img/pic3.png', price_good: 5000000, price_bad: 6000000, is_sold: false},
 				{id: 4, title: 'Урок анатомии', author: 'Рембрандт', img_href: '../img/pic4.png', is_sold: true}
 			],
-			showItems: [
-				{id: 1, title: 'Рождение Венеры', author: 'Сандро Боттичелли', img_href: '../img/pic1.png', price_good: 1000000, price_bad: 2000000, is_sold: false},
-				{id: 2, title: 'Тайная вечеря', author: 'Леонардо да Винчи', img_href: '../img/pic2.png', price_good: 3000000, is_sold: false},
-				{id: 3, title: 'Сотворение Адама', author: 'Микеланджело', img_href: '../img/pic3.png', price_good: 5000000, price_bad: 6000000, is_sold: false},
-				{id: 4, title: 'Урок анатомии', author: 'Рембрандт', img_href: '../img/pic4.png', is_sold: true}
-			],
 			query: ''
 		}
 	},
 	methods: {
 		toggle_mobile_menu: function(){
 			this.is_mobile_menu_active = !this.is_mobile_menu_active;
-		},
-		updateItems: function(){
-			console.log('ура');
-			this.showItems = this.queryItems;
 		}
 	},
 	computed: {
